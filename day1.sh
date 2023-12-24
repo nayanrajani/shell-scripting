a00e623daede12ec588e0e2bc2c6070d8704ff93
Nayan Rajani <nayan.rajani@blazeclan.com>
2023-12-24 17:55:35 +0530

    added

---
+++ day1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script just prints the name
+# This is a comment
+# Start with the variable decalaring
+
+Name="Nayan"
+
+echo "my name is ${Name}"
---
+++ day2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This Script just prints the sysinfo
+
+# display the current directory
+
+pwd
+
+
+# Display the current date and time
+
+date
+
+# display the kernel info
+
+uname -r
+uname -m
+
+#display the disk usage in human readable format
+
+df -h
